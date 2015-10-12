1c2147f867ba744a34381e779d61f20716615a14
Minh Dao Bui <minhdao.bui123@gmail.com>
2015-10-12 23:25:19 +0700

    add check box select + delete button + separate other css from app.css

--- resources/views/ads/manage.blade.php
+++ resources/views/ads/manage.blade.php
@@ -9,8 +9,8 @@
 
 @section('content')
     <br/>
-    <div class="row">
-        <table id="manage-ads" class="table table-striped table-bordered table-hover" cellspacing="0" width="100%">
+    <div class="row" id="manage-ads">
+        <table id="manage-table" class="table table-striped table-bordered table-hover" cellspacing="0" width="100%">
             <thead>
             <tr>
                 <th></th>
@@ -32,7 +32,7 @@
 @section('body-footer')
     <script type="text/javascript" src="{{asset('/datatables/datatables.min.js')}}"></script>
     <script>
-        $('#manage-ads').DataTable({
+        $('#manage-table').DataTable({
             "processing": true,
             "serverSide": true,
             "ajax": "{{url('/ads/table')}}",
@@ -42,7 +42,7 @@
                     className: 'select-checkbox',
                     defaultContent: "",
                     data: null,
-                    width:"15px"
+                    width: "15px"
                 },
                 {
                     data: 0,
@@ -86,7 +86,32 @@
                 style: 'os',
                 selector: 'td:first-child'
             },
-            order: [[ 8, 'desc' ]]
+            order: [[8, 'desc']],
+            dom: "<'row'<'col-sm-6'lB><'col-sm-6'f>>" +
+            "<'row'<'col-sm-12'tr>>" +
+            "<'row'<'col-sm-5'i><'col-sm-7'p>>",
+            lengthChange: true,
+            buttons: {
+                buttons: [
+                    {
+                        extend: 'selected',
+                        text: '<span class="glyphicon glyphicon-trash" aria-hidden="true"></span> Delete',
+                        action: function (e, dt, button, config) {
+                            var anSelected = fnGetSelected( dt );
+                            $(anSelected).remove();
+//                            dt.row('.selected').remove().draw( false );
+                        },
+                    }
+                ],
+                dom:{
+                    container:{
+                        className:'dt-buttons btn-group del-container'
+                    },
+                    button:{
+                        className:'btn btn-default'
+                    }
+                }
+            }
         });
     </script>
 @endsection
