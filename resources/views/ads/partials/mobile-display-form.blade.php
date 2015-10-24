48242e1be487234321ca8055f31e99907486d4a8
Minh Dao Bui <minhdao.bui123@gmail.com>
2015-10-24 11:27:34 +0700

    merge dao-theme to master

--- resources/views/ads/partials/mobile-display-form.blade.php
+++ resources/views/ads/partials/mobile-display-form.blade.php
@@ -1,13 +1,13 @@
 <fieldset>
     <legend>Mobile Display</legend>
     <div class="form-group">
-        {!! Form::label('title','Title',['class'=>'col-sm-3 control-label']) !!}
-        <div class="col-sm-7 col-md-6">
+        {!! Form::label('title','Title',['class'=>"$labelClass"]) !!}
+        <div class="col-sm-7">
             {!! Form::text('title',null,['class'=>'form-control','required'=>'required','minlength'=>'3','placeholder'=>'e.g. Tide Downy Washing Powder 4.5kg Discount 20%']) !!}
         </div>
     </div>
     <div class="form-group">
-        {!! Form::label('image_display','Display Type',['class'=>'col-sm-3 control-label']) !!}
+        {!! Form::label('image_display','Display Type',['class'=>"$labelClass"]) !!}
         <div class="col-sm-9">
             <label class="radio-inline">
                 {!! Form::radio('image_display',1,true,['id'=>'image_display']) !!} Image
@@ -18,8 +18,8 @@
         </div>
     </div>
     <div class="form-group" id="imageInputGroup">
-        {!! Form::label(null,'Image',['class'=>'col-sm-3 control-label']) !!}
-        <div class="col-sm-8 col-md-6">
+        {!! Form::label(null,'Image',['class'=>"$labelClass"]) !!}
+        <div class="{{$urlGroupClass}}">
             <ul class="nav nav-tabs" role="tablist">
                 <li role="presentation" class="active"><a href="#tab-image-link" aria-controls="image-link" role="tab"
                                                           data-toggle="tab">Link</a></li>
@@ -42,13 +42,13 @@
         </div>
     </div>
     <div class="form-group myHiddenInputGroup" id="webInputGroup">
-        {!! Form::label('web_url','Web Page URL',['class'=>'col-sm-3 control-label']) !!}
-        <div class="col-sm-8 col-md-7">
+        {!! Form::label('web_url','Web Page URL',['class'=>"$labelClass"]) !!}
+        <div class="{{$urlGroupClass}}">
             {!! Form::url('web_url',null,['class'=>'form-control','placeholder'=>'eg. http://example.com/ads/a100.html']) !!}
         </div>
     </div>
     <div class="form-group">
-        {!! Form::label('auto_thumbnail','Auto generate thumbnail?',['class'=>'col-sm-3 control-label']) !!}
+        {!! Form::label('auto_thumbnail','Auto generate thumbnail?',['class'=>"$labelClass"]) !!}
         <div class="col-sm-9 ">
             <div class="checkbox-inline">
                 {!! Form::checkbox('auto_thumbnail',1,true) !!}
@@ -56,10 +56,11 @@
         </div>
     </div>
     <div class="form-group " id="thumbnailInputGroup">
-        {!! Form::label(null,'Thumbnail Image',['class'=>'col-sm-3 control-label']) !!}
-        <div class="col-sm-8 col-md-6">
+        {!! Form::label(null,'Thumbnail Image',['class'=>"$labelClass"]) !!}
+        <div class="{{$urlGroupClass}}">
             <ul class="nav nav-tabs" role="tablist">
-                <li role="presentation" class="active"><a href="#tab-thumbnail-link" aria-controls="thumbnail-link" role="tab"
+                <li role="presentation" class="active"><a href="#tab-thumbnail-link" aria-controls="thumbnail-link"
+                                                          role="tab"
                                                           data-toggle="tab">Link</a></li>
                 <li role="presentation"><a href="#tab-thumbnail-upload" aria-controls="thumbnail-upload"
                                            role="tab"
