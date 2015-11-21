afe49879778766c0b966fa5c5a01c916a7432ed5
Minh Dao Bui <minhdao.bui123@gmail.com>
2015-11-21 17:40:57 +0700

    rename

--- tests/ContextAdsTestCase1.php
+++ tests/ContextAdsTestCase1.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\ActiveCustomer;
+use App\Ads;
 use App\BeaconMajor;
 use App\BeaconMinor;
+use Carbon\Carbon;
 use Faker\Factory as Faker;
 
 class ContextAdsTestCase extends ApiTestCase
@@ -43,5 +45,20 @@ class ContextAdsTestCase extends ApiTestCase
     public function extractPromotions($result)
     {
         return $result['entrancePromotions']->merge($result['aislePromotions']);
+    }
+
+    public function createBasicPromotion($overwriteFields = [])
+    {
+        $fields = array_merge([
+            'discount_value' => $this->fake->numberBetween(0, 99999999999),
+            'discount_rate' => $this->fake->numberBetween(0, 100),
+            'is_whole_system' => true,
+            'is_promotion' => true,
+            'title' => $this->fake->sentence,
+            'start_date' => Carbon::now()->subDays(1)->toDateString(),
+            'end_date' => Carbon::now()->addDays(1)->toDateString(),
+        ], $overwriteFields);
+
+        return Ads::create($fields);
     }
 }
